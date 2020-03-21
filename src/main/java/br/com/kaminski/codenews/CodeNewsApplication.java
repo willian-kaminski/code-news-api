864ac62315d5645668b680230871fb6e449ec312
willian-kaminsnki <developer.williansantos@gmail.com>
2020-03-21 19:53:44 -0300

    add maven project

---
+++ src/main/java/br/com/kaminski/codenews/CodeNewsApplication.java
@@ -0,0 +1,13 @@
+package br.com.kaminski.codenews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kaminski/codenews/CodeNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kaminski.codenews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
