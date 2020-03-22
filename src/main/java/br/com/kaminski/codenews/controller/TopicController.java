70477c5fefc6f13ced1f31a378ec215d3bcf4b6f
willian-kaminsnki <developer.williansantos@gmail.com>
2020-03-22 17:29:01 -0300

    adding TopicController class

--- src/main/java/br/com/kaminski/codenews/controller/TopicController.java
+++ src/main/java/br/com/kaminski/codenews/controller/TopicController.java
@@ -2,18 +2,23 @@ package br.com.kaminski.codenews.controller;
 
 import br.com.kaminski.codenews.controller.dto.TopicDto;
 import br.com.kaminski.codenews.domain.Topic;
+import br.com.kaminski.codenews.repository.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicRepository topicRepository;
+
     @RequestMapping("/topicos")
     public List<TopicDto> lis(){
-        Topic topic = new Topic((long) 1, "Corona");
-        return TopicDto.convertTopic(Arrays.asList(topic, topic));
+        List<Topic> topics = topicRepository.findAll();
+        return TopicDto.convertTopic(topics);
     }
+
 }
