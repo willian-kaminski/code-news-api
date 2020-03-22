ef8fc9b90cca3fca8b0f7baf9aa926ea65b0f8b0
willian-kaminsnki <developer.williansantos@gmail.com>
2020-03-21 21:41:34 -0300

    adding improvements in Topic class

--- src/main/java/br/com/kaminski/codenews/domain/Topic.java
+++ src/main/java/br/com/kaminski/codenews/domain/Topic.java
@@ -1,9 +1,12 @@
 package br.com.kaminski.codenews.domain;
 
+import java.time.LocalDateTime;
+
 public class Topic {
 
     private Long id;
     private String name;
+    private LocalDateTime localDateTimePublication;
 
     public Topic(Long id, String name) {
         this.id = id;
@@ -24,5 +27,13 @@ public class Topic {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getLocalDateTimePublication() {
+        return localDateTimePublication;
+    }
+
+    public void setLocalDateTimePublication(LocalDateTime localDateTimePublication) {
+        this.localDateTimePublication = localDateTimePublication;
     }
 }
